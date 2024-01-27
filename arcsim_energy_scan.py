1fc3f1c57ef8b1d0f1f1593e4c3063c80f9ed6c8
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2024-01-27 19:23:53 +0100

    python script to launch simulations

---
+++ arcsim_energy_scan.py
@@ -0,0 +1,13 @@
+# energy_list = [x for x in range(1,10,2)] + [x for x in range(10,60,20)]
+energy_list = [2,5,7,10,15,50]
+import os
+
+multiplicity = 100
+particle = 'pi+'
+
+for energy_GeV in energy_list:
+    outputFile = f'arcsim_{particle}_{energy_GeV}GeV.root'
+    cmd = f"nohup ddsim --steeringFile steering.py --gun.energy '{energy_GeV}*GeV' --gun.multiplicity {multiplicity} --gun.particle '{particle}' --outputFile {outputFile} &"
+
+    os.system( cmd )
+
